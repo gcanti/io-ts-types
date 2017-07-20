cdb34e3f4d4fab6f481844b4c5dbfde28fc06380
gcanti <giulio.canti@gmail.com>
2017-07-20 19:26:05 +0200

    createOptionFromUndefined: handle null too

--- src/fp-ts/createOptionFromUndefined.ts
+++ src/fp-ts/createOptionFromUndefined.ts
@@ -2,5 +2,5 @@ import * as t from 'io-ts'
 import { Option, none, some } from 'fp-ts/lib/Option'
 
 export function createOptionFromUndefined<A>(type: t.Type<A>): t.Type<Option<A>> {
-  return t.mapWithName(v => (v === undefined ? none : some(v)), t.union([type, t.undefined]), `Option<${type.name}>`)
+  return t.mapWithName(v => (v == null ? none : some(v)), t.union([type, t.undefined]), `Option<${type.name}>`)
 }
