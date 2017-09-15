dc27391eef1601bc1c0b7c97acb9f1f0fc08d7a7
Lee Henson <lee.m.henson@gmail.com>
2017-09-15 11:50:15 +0100

    s/lensesFromInterface/lensesFromProps/

--- src/monocle-ts/lensesFromProps.ts
+++ src/monocle-ts/lensesFromProps.ts
@@ -1,11 +1,11 @@
 import * as t from 'io-ts'
 import { Lens } from 'monocle-ts'
 
-export type LensesFromInterface<P extends t.Props, T extends t.InterfaceOf<P> = t.InterfaceOf<P>> = {
+export type LensesFromProps<P extends t.Props, T extends t.InterfaceOf<P> = t.InterfaceOf<P>> = {
   [K in keyof P]: Lens<T, T[K]>
 }
 
-export function lensesFromInterface<P extends t.Props>(i: t.InterfaceType<P>): LensesFromInterface<P> {
+export function lensesFromProps<P extends t.Props>(i: t.InterfaceType<P>): LensesFromProps<P> {
   const r: any = {}
   for (const k in i.props) {
     r[k] = Lens.fromProp<any, typeof k>(k)
