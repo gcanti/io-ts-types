66aeeec50f10c039fd2a7efe5f561f3609a144ab
Lee Henson <lee.m.henson@gmail.com>
2017-09-21 10:54:22 +0100

    alternative names for fromInterface  (#4)
    * add fromInterface to get lenses from an interface
    
    * prefix fromInterface to lensesFromInterface
    
    * prettier
    
    * s/lensesFromInterface/lensesFromProps/

---
+++ src/monocle-ts/lensesFromProps.ts
@@ -0,0 +1,14 @@
+import * as t from 'io-ts'
+import { Lens } from 'monocle-ts'
+
+export type LensesFromProps<P extends t.Props, T extends t.InterfaceOf<P> = t.InterfaceOf<P>> = {
+  [K in keyof P]: Lens<T, T[K]>
+}
+
+export function lensesFromProps<P extends t.Props>(i: t.InterfaceType<P>): LensesFromProps<P> {
+  const r: any = {}
+  for (const k in i.props) {
+    r[k] = Lens.fromProp<any, typeof k>(k)
+  }
+  return r
+}
