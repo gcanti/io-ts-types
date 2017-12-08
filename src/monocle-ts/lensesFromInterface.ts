e15becf3cb247da118be02b72791c556934d6e04
gcanti <giulio.canti@gmail.com>
2017-12-08 09:37:25 +0100

    upgrade to latest `io-ts@0.9.1`

--- src/monocle-ts/lensesFromInterface.ts
+++ src/monocle-ts/lensesFromInterface.ts
@@ -1,6 +1,6 @@
 import * as t from 'io-ts'
 import { lensesFromProps, LensesFromProps } from './lensesFromProps'
 
-export const lensesFromInterface = <P extends t.Props>(i: t.InterfaceType<P>): LensesFromProps<P> => {
+export const lensesFromInterface = <P extends t.Props>(i: t.InterfaceType<P, t.InterfaceOf<P>>): LensesFromProps<P> => {
   return lensesFromProps(i.props)
 }
