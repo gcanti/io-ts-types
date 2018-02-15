8754f7b68a2e9b932cd8e89864258ce0dc41555d
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2018-02-15 15:30:24 +0100

    add createNonEmptyArrayFromArray (fixes: #18)

---
+++ src/fp-ts/createNonEmptyArrayFromArray.ts
@@ -0,0 +1,15 @@
+import * as t from 'io-ts'
+import { NonEmptyArray, fromArray } from 'fp-ts/lib/NonEmptyArray'
+
+const getNonEmptyArrayFromArray = <A = never>() =>
+  new t.Type<NonEmptyArray<A>, Array<A>, Array<A>>(
+    'NonEmptyArrayFromArray',
+    (m): m is NonEmptyArray<A> => m instanceof NonEmptyArray,
+    (as, c) => fromArray(as).foldL(() => t.failure(as, c), t.success),
+    nep => nep.toArray()
+  )
+
+export const createNonEmptyArrayFromArray = <RT extends t.Mixed>(
+  type: RT
+): t.Type<NonEmptyArray<t.TypeOf<RT>>, t.OutputOf<RT>[], t.mixed> =>
+  t.array(type).pipe(getNonEmptyArrayFromArray(), `NonEmptyArray<${type.name}>`)
