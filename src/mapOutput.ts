d2f67df981eae11ed6b6e73e6d58238d614ccd17
gcanti <giulio.canti@gmail.com>
2018-03-06 07:49:38 +0100

    Polish: remove typeof ... "undefined"

--- src/mapOutput.ts
+++ src/mapOutput.ts
@@ -2,4 +2,4 @@ import { Type } from 'io-ts'
 
 /** Changes the output type of the given runtime type  */
 export const mapOutput = <A, O, I, P>(type: Type<A, O, I>, f: (p: O) => P, name?: string): Type<A, P, I> =>
-  new Type(typeof name === 'undefined' ? type.name : name, type.is, type.validate, a => f(type.encode(a)))
+  new Type(name === undefined ? type.name : name, type.is, type.validate, a => f(type.encode(a)))
