9237f2994463da3ffaafcc9c3291c14b8037f021
gcanti <giulio.canti@gmail.com>
2018-05-02 18:34:27 +0200

    Internal: simplify lensesFromInterface typings, closes #37

--- src/monocle-ts/lensesFromProps.ts
+++ src/monocle-ts/lensesFromProps.ts
@@ -1,11 +1,11 @@
 import * as t from 'io-ts'
 import { Lens } from 'monocle-ts'
 
-export type LensesFromProps<P extends t.Props, T extends t.TypeOfProps<P> = t.TypeOfProps<P>> = {
-  [K in keyof P]: Lens<T, T[K]>
-}
+export type LensesFromProps<P extends t.Props> = { [K in keyof P]: Lens<t.TypeOfProps<P>, t.TypeOfProps<P>[K]> }
 
-export const lensesFromProps = <P extends t.Props>(props: P): LensesFromProps<P> => {
+export const lensesFromProps = <P extends t.Props>(
+  props: P
+): { [K in keyof P]: Lens<t.TypeOfProps<P>, t.TypeOfProps<P>[K]> } => {
   const r: any = {}
   for (const k in props) {
     r[k] = Lens.fromProp<any, typeof k>(k)
