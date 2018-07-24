a8fc394fba3d48112761d46eacc845f1624a08b4
gcanti <giulio.canti@gmail.com>
2018-07-24 07:41:25 +0200

    fix generated declarations, closes #46

--- src/newtype-ts/fromNewtype.ts
+++ src/newtype-ts/fromNewtype.ts
@@ -5,6 +5,6 @@ export const fromNewtype: <N extends AnyNewtype = never>(
   type: Type<CarrierOf<N>, CarrierOf<N>, mixed>
 ) => Type<N, CarrierOf<N>, mixed> = type => type as any
 
-export const fromNewtypeCurried = <N extends AnyNewtype = never>() => <O>(
+export const fromNewtypeCurried: <N extends AnyNewtype = never>() => <O>(
   type: Type<CarrierOf<N>, O, mixed>
-): Type<N, O, mixed> => type as any
+) => Type<N, O, mixed> = () => type => type as any
--- src/newtype-ts/fromRefinement.ts
+++ src/newtype-ts/fromRefinement.ts
@@ -2,11 +2,15 @@ import { AnyNewtype, CarrierOf } from 'newtype-ts'
 import { Prism } from 'monocle-ts'
 import { Type, mixed, success, failure } from 'io-ts'
 
-export const fromRefinement = <S extends AnyNewtype = never>() => <O>(
+export const fromRefinement: <S extends AnyNewtype = never>() => <O>(
+  carrier: Type<CarrierOf<S>, O, mixed>,
+  prism: Prism<CarrierOf<S>, S>,
+  name?: string
+) => Type<S, O, mixed> = <S extends AnyNewtype = never>() => <O>(
   carrier: Type<CarrierOf<S>, O, mixed>,
   prism: Prism<CarrierOf<S>, S>,
   name: string = `Refinement<${carrier.name}>`
-): Type<S, O, mixed> =>
+) =>
   new Type(
     name,
     (m): m is S => carrier.is(m) && prism.getOption(m).isSome(),
