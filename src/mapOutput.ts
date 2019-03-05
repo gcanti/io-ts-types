b36b5f69f09d8ca3024cbe483a6864da361a453e
gcanti <giulio.canti@gmail.com>
2019-03-05 09:48:47 +0100

    mapOutput: small refactoring

--- src/mapOutput.ts
+++ src/mapOutput.ts
@@ -20,5 +20,6 @@ import { Type } from 'io-ts'
  * assert.strictEqual(Output.encode(none), undefined)
  * assert.strictEqual(Output.encode(some(1)), 1)
  */
-export const mapOutput = <A, O, I, P>(codec: Type<A, O, I>, f: (p: O) => P, name?: string): Type<A, P, I> =>
-  new Type(name === undefined ? codec.name : name, codec.is, codec.validate, a => f(codec.encode(a)))
+export function mapOutput<A, O, I, P>(codec: Type<A, O, I>, f: (p: O) => P, name: string = codec.name): Type<A, P, I> {
+  return new Type(name, codec.is, codec.validate, a => f(codec.encode(a)))
+}
