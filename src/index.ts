67f5ca83c8a948d627ab987ec5f71961e357255d
gcanti <giulio.canti@gmail.com>
2019-03-12 09:57:40 +0100

    New Feature: add fromRefinement

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,6 @@ export { DateFromISOString, DateFromISOStringC, DateFromISOStringType } from './
 export { DateFromNumber, DateFromNumberC, DateFromNumberType } from './Date/DateFromNumber'
 export { DateFromUnixTime, DateFromUnixTimeC, DateFromUnixTimeType } from './Date/DateFromUnixTime'
 
-// RegExp
-export { regexp, RegExpC, RegExpType } from './regexp'
-
 // fp-ts
 export { createEitherFromJSON, EitherFromJSONC, EitherFromJSONType } from './fp-ts/createEitherFromJSON'
 export {
