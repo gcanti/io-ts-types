a0f8b4f0944e78ab637e8e10c3521bbc86230260
gcanti <giulio.canti@gmail.com>
2019-03-25 07:10:17 +0100

    New Feature: add NonEmptyString

--- docs/modules/withMessage.ts.md
+++ docs/modules/withMessage.ts.md
@@ -1,6 +1,6 @@
 ---
 title: withMessage.ts
-nav_order: 29
+nav_order: 30
 parent: Modules
 ---
 
---
+++ test/NonEmptyString.ts
@@ -0,0 +1,33 @@
+import * as assert from 'assert'
+import { NonEmptyString } from '../src/NonEmptyString'
+import { assertSuccess, assertFailure } from './helpers'
+
+describe('NonEmptyString', () => {
+  describe('is', () => {
+    it('should check a isomorphic value', () => {
+      const T = NonEmptyString
+      assert.strictEqual(T.is(''), false)
+      assert.strictEqual(T.is('a'), true)
+    })
+  })
+
+  describe('decode', () => {
+    it('should succeed validating a valid value', () => {
+      const T = NonEmptyString
+      assertSuccess(T.decode('a'), 'a' as NonEmptyString)
+    })
+
+    it('should fail validation an invalid value', () => {
+      const T = NonEmptyString
+      assertFailure(T, null, ['Invalid value null supplied to : NonEmptyString'])
+      assertFailure(T, '', ['Invalid value "" supplied to : NonEmptyString'])
+    })
+  })
+
+  describe('encode', () => {
+    it('should encode a isomorphic value', () => {
+      const T = NonEmptyString
+      assert.strictEqual(T.encode('a' as NonEmptyString), 'a')
+    })
+  })
+})
